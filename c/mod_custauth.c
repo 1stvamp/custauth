537a24464ff9c08ed62188019f7deae9b6a623cd
Wesley Mason <wes@toottoot.(none)>
2009-05-13 17:51:54 +0100

    Added basic mod_custauth.c from module template

---
+++ c/mod_custauth.c
@@ -0,0 +1,134 @@
+/*
+	Copyright 2008 Wesley Mason (wes@1stvamp.org)
+	Base on the basic Apache module tutorial "mod_tut1" & "mod_tut2" by Kevin O'Donnell
+	<http://threebit.net/tutorials/>
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/*
+ * Include the core server components.
+ */
+#include "httpd.h"
+#include "http_config.h"
+
+/*
+ * The default value for the error string.
+ */
+#ifndef DEFAULT_MODCUSTAUTH_ERRSTRING
+#define DEFAULT_MODCUSTAUTH_ERRSTRING "apache2_mod_custauth: config error"
+#endif
+
+/*
+ * This module
+ */
+module AP_MODULE_DECLARE_DATA custauth_module;
+
+/*
+ * This modules per-server configuration structure.
+ */
+typedef struct {
+	char *string;
+} modcustauth_config;
+
+/*
+ * This function is registered as a handler for HTTP Auth methods and will
+ * therefore be invoked for Basic and Digest Auth requests
+ */
+static int mod_custauth_auth_handler (request_rec *r)
+{
+	// Get the module configuration
+	modcustauth_config *s_cfg = ap_get_module_config(r->server->module_config, &custauth_module);
+	
+	// Send a message to stderr (apache redirects this to the error log)
+	fprintf(stderr,"apache2_mod_custauth: Auth request by.\n");
+	return DECLINED;
+}
+
+/*
+ * This function is a callback and it declares what other functions
+ * should be called for request processing and configuration requests.
+ * This callback function declares the Handlers for other events.
+ */
+static void mod_custauth_register_hooks (apr_pool_t *p)
+{
+	// I think this is the call to make to register a handler for method calls (GET PUT et. al.).
+	// We will ask to be last so that the comment has a higher tendency to
+	// go at the end.
+	ap_hook_handler(mod_custauth_auth_handler, NULL, NULL, APR_HOOK_FIRST);
+}
+/**
+ * This function is called when the "ModTut2String" configuration directive is parsed.
+ */
+static const char *set_modcustauth_string(cmd_parms *parms, void *mconfig, const char *arg)
+{
+	// get the module configuration (this is the structure created by create_modcustauth_config())
+	modcustauth_config *s_cfg = ap_get_module_config(parms->server->module_config, &custauth_module);
+
+	// make a duplicate of the argument's value using the command parameters pool.
+	s_cfg->string = (char *) arg;
+
+	// success
+	return NULL;
+}
+
+/**
+ * A declaration of the configuration directives that are supported by this module.
+ */
+static const command_rec mod_custauth_cmds[] =
+{
+	AP_INIT_TAKE1(
+		"ModuleTutorialString",
+		set_modcustauth_string,
+		NULL,
+		RSRC_CONF,
+		"ModTut2String <string> -- the string to print to the error log for each HTTP request."
+	),
+	{NULL}
+};
+
+/**
+ * Creates the per-server configuration records.
+ */
+static void *create_modcustauth_config(apr_pool_t *p, server_rec *s)
+{
+	modcustauth_config *newcfg;
+
+	// allocate space for the configuration structure from the provided pool p.
+	newcfg = (modcustauth_config *) apr_pcalloc(p, sizeof(modcustauth_config));
+
+	// set the default value for the error string.
+	newcfg->string = DEFAULT_MODTUT2_STRING;
+
+	// return the new server configuration structure.
+	return (void *) newcfg;
+}
+
+/*
+ * Declare and populate the module's data structure.  The
+ * name of this structure ('custauth_module') is important - it
+ * must match the name of the module.  This structure is the
+ * only "glue" between the httpd core and the module.
+ */
+module AP_MODULE_DECLARE_DATA custauth_module =
+{
+	STANDARD20_MODULE_STUFF, // standard stuff; no need to mess with this.
+	NULL, // create per-directory configuration structures - we do not.
+	NULL, // merge per-directory - no need to merge if we are not creating anything.
+	create_modcustauth_config, // create per-server configuration structures.
+	NULL, // merge per-server - hrm - examples I have been reading don't bother with this for trivial cases.
+	mod_custauth_cmds, // configuration directive handlers
+	mod_custauth_register_hooks, // request handlers
+};
